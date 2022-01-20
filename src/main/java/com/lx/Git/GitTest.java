549922dfb5bb51ec0b266e01d99f504c96f6e1e9
malaxiaohuoguo <97959981+malaxiaohuoguo@users.noreply.github.com>
2022-01-20 23:09:10 +0800

    111test

--- src/main/java/com/lx/Git/GitTest.java
+++ src/main/java/com/lx/Git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
-        111
+        System.out.println("111 test!");
     }
 }

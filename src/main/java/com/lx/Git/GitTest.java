b545ebdf2e7adddbd5ff2a3f196b3b9d87e4cbc0
malaxiaohuoguo <97959981+malaxiaohuoguo@users.noreply.github.com>
2022-01-21 00:32:58 +0800

    pull test2 commit

--- src/main/java/com/lx/Git/GitTest.java
+++ src/main/java/com/lx/Git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("push test1!");
         System.out.println("pull test1!");
         System.out.println("push test2!");
+        System.out.println("pull test2!");
     }
 }
